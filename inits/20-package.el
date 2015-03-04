e258907dad8e8ea434cccf40b4485aa8e88c13a5
shiki49 <shiki.49.313@gmail.com>
2015-03-04 15:50:16 +0900

    update

--- inits/20-package.el
+++ inits/20-package.el
@@ -70,6 +70,31 @@
 (require 'projectile-rails)
 (add-hook 'projectile-mode-hook 'projectile-rails-on)
 
+(defun projectile-rails-goto-template-at-point ()
+  (interactive)
+  (let* ((template (projectile-rails-filename-at-point))
+         (dir (projectile-rails-template-dir template))
+         (name (projectile-rails-template-name template))
+         (format (projectile-rails-template-format template)))
+    (if format
+        (loop for processor in '("erb" "haml" "slim")
+              for template = (s-lex-format "${dir}${name}.${format}.${processor}")
+              for partial = (s-lex-format "${dir}_${name}.${format}.${processor}")
+              for partial-2 = (expand-file-name
+                               (s-lex-format "_${name}.${format}.${processor}")
+                               (projectile-expand-root "app/views/application"))
+              for partial-3 = (expand-file-name
+                               (s-lex-format "_${name}.${format}.${processor}")
+                               (projectile-expand-root "app/views/shared"))
+              until (or
+                     (projectile-rails-ff template)
+                     (projectile-rails-ff partial)
+                     (projectile-rails-ff partial-2)
+                     (projectile-rails-ff partial-3)))
+      (message "Could not recognize the template's format")
+            (dired dir))))
+
+
 ;;enh-ruby
 (autoload 'enh-ruby-mode "enh-ruby-mode"
   "Mode for editing ruby source files" t)
