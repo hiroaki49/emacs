3391e2d56d481f64fd0ee966c7489bef2753f8a3
shiki49 <shiki49.com@gmail.com>
2015-10-29 23:14:31 +0900

    rm .cask file

--- .cask/24.4.1/elpa/inf-php-20130414.21/inf-php-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "inf-php" "20130414.21" "Run a php interactive shell in a buffer" '((php-mode "1.5.0")) :url "https://github.com/taksatou/inf-php" :keywords '("languages" "php"))
--- .cask/24.4.1/elpa/php-completion-20140728.859/php-completion-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "php-completion" "20140728.859" "No description available." 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
